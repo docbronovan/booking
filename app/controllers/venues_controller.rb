42d60a0c60726f788792d2235713a4bb42d6edda
docbronovan <brockdonovan@gmail.com>
2015-09-09 17:10:28 -0400

    venue#about done

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,6 +2,11 @@ class VenuesController < ApplicationController
   def index
   end
 
+  def about
+    @venue = Venue.find(params[:id])
+    @user = @venue.user_id
+  end
+
   def show
     @venue = Venue.find(params[:id])
     @events = Event.all

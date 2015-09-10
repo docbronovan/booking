b79f195de69cc4cef90d9338ea2beb53ebc96107
docbronovan <brockdonovan@gmail.com>
2015-09-10 17:43:11 -0400

    remove after signup path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # set action mailer
+  config.action_mailer.default_url_options = { host: 'brock-donovan-blocmarks.herokuapp.com'}
+
 end

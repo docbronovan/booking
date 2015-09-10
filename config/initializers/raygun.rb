52a7412d5f42446b0e71be240a077fc6cb220fdd
docbronovan <brockdonovan@gmail.com>
2015-09-10 17:21:23 -0400

    add raygun

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -2,6 +2,6 @@ Raygun.setup do |config|
   config.api_key = "9V6rjtkMHl/CsyyY7dTYHA=="
   config.filter_parameters = Rails.application.config.filter_parameters
 
-  The default is Rails.env.production?
-  config.enable_reporting = !Rails.env.development? && !Rails.env.test?
+  # The default is Rails.env.production?
+  #config.enable_reporting = !Rails.env.development? && !Rails.env.test?
 end

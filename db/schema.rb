74570db7ded89c2ef02fa2d61618515962769a00
docbronovan <brockdonovan@gmail.com>
2015-10-12 14:20:59 -0400

    contact form complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010214442) do
+ActiveRecord::Schema.define(version: 20151012135953) do
 
   create_table "bands", force: :cascade do |t|
     t.integer  "user_id"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20151010214442) do
   end
 
   add_index "bands", ["user_id"], name: "index_bands_on_user_id"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
+    t.string   "nickname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.integer  "venue_id"
